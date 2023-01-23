e01dadb48c356ee0ded5dac6bec779b7d666f6a9
Linus Lee <linus@thesephist.com>
2023-01-23 04:42:03 -0500

    feat: Modernize codebase

--- service/app.py
+++ service/app.py
@@ -4,28 +4,25 @@ from transformers import pipeline, set_seed, AutoTokenizer, AutoModelForCausalLM
 
 model_name = 'EleutherAI/gpt-j-6B'
 
-# tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-# model
-model = AutoModelForCausalLM.from_pretrained(model_name)
-model.config.temperature = 0.75
-model.config.top_p = 0.9
-
-# generator pipeline
-generator = pipeline('text-generation',
-        tokenizer=tokenizer,
-        model=model)
-
-set_seed(int(time.time()))
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map='auto', torch_dtype=torch.float16)
+generator = pipeline(
+    'text-generation',
+    tokenizer=tokenizer,
+    model=model,
+)
 
 def infer(prompt, tokens_count, num_sequences, eos_token):
-    input_tokens = len(tokenizer(prompt)['input_ids'])
-    seqs = generator(prompt,
-            pad_token_id=tokenizer.eos_token_id,
-            eos_token_id=tokenizer(eos_token).input_ids[0] if eos_token else tokenizer.eos_token_id,
-            max_length=input_tokens + tokens_count,
-            num_return_sequences=num_sequences)
+    seqs = generator(
+        prompt,
+        pad_token_id=tokenizer.eos_token_id,
+        eos_token_id=tokenizer(eos_token).input_ids[0] if eos_token else tokenizer.eos_token_id,
+        max_new_tokens=tokens_count,
+        num_return_sequences=num_sequences,
+        do_sample=True,
+        temperature=1.0,
+        top_p=0.9,
+    )
     return jsonify([seq['generated_text'] for seq in seqs])
 
 app = Flask(__name__)
@@ -34,6 +31,10 @@ app.config['JSON_AS_ASCII'] = False
 @app.route('/generate/', methods=['POST'])
 def generate():
     params = request.get_json(force=True)
-    # TODO: error handling
-    return infer(params.get('text'), params.get('tokens', 10), params.get('n', 1), params.get('eos'))
+    return infer(
+        params.get('text', ''),
+        params.get('tokens', 10),
+        params.get('n', 1),
+        params.get('eos'),
+    )
 
